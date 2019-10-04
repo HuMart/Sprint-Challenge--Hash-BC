ce1c3b434bc2f10c397042ecf4598ff906ba6df7
HuMart <manya01@protonmail.com>
2019-10-04 11:29:10 -0400

    testing miner.py on test-server

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -26,6 +26,8 @@ def proof_of_work(last_proof):
     print("Searching for next proof")
     proof = 0
     #  TODO: Your code here
+    while valid_proof(last_proof, proof) is False:
+        proof += 1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
@@ -40,7 +42,9 @@ def valid_proof(last_hash, proof):
     """
 
     # TODO: Your code here!
-    pass
+    guess = f"{proof}".encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return str(last_hash)[:6] == guess_hash[6:]
 
 
 if __name__ == '__main__':
@@ -48,8 +52,8 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         node = sys.argv[1]
     else:
-        node = "https://lambda-coin.herokuapp.com/api"
-
+        # node = "https://lambda-coin.herokuapp.com/api"
+        node = "https://lambda-coin-test-1.herokuapp.com/api"
     coins_mined = 0
 
     # Load or create ID
@@ -59,8 +63,12 @@ if __name__ == '__main__':
     f.close()
 
     if id == 'NONAME\n':
-        print("ERROR: You must change your name in `my_id.txt`!")
-        exit()
+        f = open("my_id.txt", "w")
+        id = str(uuid4()).replace('-','')
+        print("Created new ID")
+        f.write(id)
+        f.close()
+        # exit()
     # Run forever until interrupted
     while True:
         # Get the last proof from the server
